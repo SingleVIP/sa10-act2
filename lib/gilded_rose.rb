bf03e9f442232f9a4054f16613d02b6f0c901007
peter <niqixian@gmail.com>
2024-02-27 20:24:07 -0600

    Back to green now to refactor

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -58,11 +58,13 @@ class GildedRose
   end
 
   def normal_tick
-    if @days_remaining > 0
-      @quality -= 1
-    end
-    if @days_remaining <= 0
-      @quality -= 2
+    if @quality != 0
+      if @days_remaining > 0
+        @quality -= 1
+      end
+      if @days_remaining <= 0
+        @quality -= 2
+      end
     end
     @days_remaining -= 1
   end
